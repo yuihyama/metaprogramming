0a4beef6d7c5a33a767e44ea9417692d31dbcefa
yuihyama <yuihyama@gmail.com>
2018-08-21 00:49:04 +0900

    Update

--- test/around_alias_test.rb
+++ test/around_alias_test.rb
@@ -1,19 +1,26 @@
 #!/usr/bin/env ruby
 
-require 'test/unit'
+require 'minitest/autorun'
 require './around_alias'
 
-class AroundAliasTest < Test::Unit::TestCase
+class AroundAliasTest < Minitest::Test
   def test_next_alpha
     assert_equal 'B', 'A'.next_alpha
+    assert_equal 'C', 'B'.next_alpha
+    assert_equal 'b', 'a'.next_alpha
+    assert_equal 'c', 'b'.next_alpha
+
+    assert_output("\"B\"\n") { p 'A'.next_alpha }
+    assert_output("B\n") { puts 'A'.next_alpha }
+    assert_output("C\n") { puts 'B'.next_alpha }
+    assert_output("b\n") { puts 'a'.next_alpha }
+    assert_output("c\n") { puts 'b'.next_alpha }
   end
 end
 
-# Minitest:
-#
-# require 'minitest/autorun'
-#
-# class AroundAliasTest < Minitest::Test
+# Test::Unit
+# require 'test/unit'
+# class AroundAliasTest < Test::Unit::TestCase
 #   def test_next_alpha
 #     assert_equal 'B', 'A'.next_alpha
 #   end
