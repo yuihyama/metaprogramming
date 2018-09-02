667544b6d4d5c9dd83d6abfb23bbbf8fd6e67f41
yuihyama <yuihyama@gmail.com>
2018-09-02 14:00:15 +0900

    Update

--- test/code_processor_test.rb
+++ test/code_processor_test.rb
@@ -7,5 +7,7 @@ class CodeProcessorTest < Minitest::Test
   def test_code_processor
     assert code_processor
     assert_equal ["1 + 1\n", "2 * 2\n"], code_processor
+
+    assert_output("1 + 1 # => 2\n2 * 2 # => 4\n") { code_processor }
   end
 end
